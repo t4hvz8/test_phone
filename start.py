37dfd59422161f2af173a509e5e6845d7a441622
DV_r2d2 <99878145+t4hvz8@users.noreply.github.com>
2026-03-13 13:59:10 +0600

    Add files via upload

--- start.py
+++ start.py
@@ -2,7 +2,6 @@
 # установить pip install -r requirements.txt
 
 
-
 import datetime
 import asyncio
 import sqlite3
@@ -23,7 +22,6 @@ from datetime import datetime
 bot = Bot(token='7139072705:AAFmOzwzRlSRAIJvcUdem8Tjw0wseGPFJkg')
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
-
 
     
 class user_message(StatesGroup):
@@ -47,22 +45,21 @@ with sqlite3.connect('test.db') as con:
 async def start(message: types.Message, state: FSMContext):
     if state:
         await state.clear()
+    user_id = message.from_user.id
     name = message.chat.first_name
+    print(f"{user_id} нажал старт")
     await state.set_state(user_message.save)
     await message.answer (f"Привет, {name}!\nВводи мне текст или фото и я сохраню", parse_mode="HTML")
 
 @dp.message(Command("file"))
 async def start(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
+    print(f"{user_id} запросил файл БД")
     if state:
         await state.clear()
     path = f'test.db'
     document = FSInputFile(path)
     await bot.send_document(chat_id=user_id, document=document, caption="файл базы", parse_mode="HTML")
-
-    name = message.chat.first_name
-    await state.set_state(user_message.save)
-    await message.answer (f"Привет, {name}!\nВводи мне текст или фото и я сохраню", parse_mode="HTML")
 
 @dp.callback_query()
 async def process_callback(callback_query: types.CallbackQuery, state: FSMContext):
@@ -73,6 +70,7 @@ async def process_callback(callback_query: types.CallbackQuery, state: FSMContex
         await callback_query.answer()
         if state:
             await state.clear()
+        print(f"{user_id} нажал ОК")
         name = callback_query.from_user.first_name
         await bot.send_message(
             chat_id=user_id,  
@@ -85,14 +83,17 @@ async def finish_task(message: Message, state: FSMContext):
     user_id = message.from_user.id
     current_time = datetime.now().strftime("%d.%m.%Y %H:%M")
     if is_image_message(message):
+        print(f"{user_id} прислал картинку")
         # Пользователь прислал фото
         if message.caption:
+            print(f"{user_id} у картинки есть описание")
             # Есть описание к фото - сохраняем описание в .txt
             description = message.caption
-            with sqlite3.connect(f'test.db') as con:
+            with sqlite3.connect('test.db') as con:
                 cur = con.cursor()
                 cur.execute(f'INSERT INTO data (us_idtg, us_text, us_datetime) VALUES (?, ?, ?)', (user_id, description, current_time))
                 con.commit()
+                print("Записал описание в БД")
         # Сохраняем фото
         photo = message.photo[-1]  # Берем фото с самым высоким разрешением
         file_id = photo.file_id
@@ -120,12 +121,13 @@ async def finish_task(message: Message, state: FSMContext):
                 reply_markup=board.as_markup()
             )
     elif message.text:
-        
+        print(f"{user_id} прислал текст")
         description = message.text
         with sqlite3.connect('test.db') as con:
             cur = con.cursor()
             cur.execute(f'INSERT INTO data (us_idtg, us_text, us_datetime) VALUES (?, ?, ?)', (user_id, description, current_time))
             con.commit()
+            print(f"текст записан")
         board = InlineKeyboardBuilder()
         board.add(types.InlineKeyboardButton(text="OK", callback_data="OK")) 
         await bot.send_message(
@@ -143,9 +145,12 @@ def save_finish_photo(user_id):
         cur = con.cursor()
         done = cur.execute(f'INSERT INTO data (us_blob) VALUES (?)', [photo])
         con.commit()
+        
     if done:
+        print("Записал картинку в БД")
         return True
     else:
+        print("Не записал картинку в БД")
         return False
 
 
